51a753921a2ded7663b3b9ef5233f262cee47f56
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-17 11:55:31 -0300

    Aplicar correccion de calculo en editar pedido

--- resources/views/pedido_compras/edit.blade.php
+++ resources/views/pedido_compras/edit.blade.php
@@ -37,6 +37,7 @@
                     @endphp
 
                     @include('pedido_compras.fields')
+                    @include('pedido_compras.calculo_detalle_fix')
 
                 </div>
             </div>
