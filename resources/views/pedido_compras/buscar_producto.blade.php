47cd704557b5f9107899be92f2d978ce545d96f4
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-18 11:29:37 -0300

    Corregir normalizacion de precio en buscador de productos

--- resources/views/pedido_compras/buscar_producto.blade.php
+++ resources/views/pedido_compras/buscar_producto.blade.php
@@ -19,7 +19,26 @@
             @php
                 $stockGeneral = (float) ($product->stock_general ?? 0);
                 $stockDisponible = (float) ($product->stock_disponible_pedir ?? 0);
-                $precioPedido = (float) ($product->prec_vent ?? 0);
+
+                // Mantener el precio real aunque venga formateado como 75.000 o 75.000,00.
+                $precioRaw = trim((string) ($product->prec_vent ?? '0'));
+
+                if (str_contains($precioRaw, ',') && str_contains($precioRaw, '.')) {
+                    // Formato PY: 75.000,00
+                    $precioNormalizado = str_replace('.', '', $precioRaw);
+                    $precioNormalizado = str_replace(',', '.', $precioNormalizado);
+                } elseif (str_contains($precioRaw, ',')) {
+                    // 75000,00
+                    $precioNormalizado = str_replace(',', '.', $precioRaw);
+                } elseif (preg_match('/^\d{1,3}(\.\d{3})+$/', $precioRaw)) {
+                    // 75.000 / 1.250.000 => separador de miles
+                    $precioNormalizado = str_replace('.', '', $precioRaw);
+                } else {
+                    // 75000 / 75000.00
+                    $precioNormalizado = $precioRaw;
+                }
+
+                $precioPedido = (float) $precioNormalizado;
                 $puedePedir = $stockDisponible > 0;
             @endphp
 
